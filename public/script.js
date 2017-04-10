fb775c2b3e0fa3f256308c2aa6bbc53f81659bf8
Noah Heinrich <noahheinrich@gmail.com>
2017-04-10 10:49:52 -0500

    added data retrieval to jquery

--- public/script.js
+++ public/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+  firebase.database().ref('ads').once('value').then(function(snapshot){
+    var ad_list = snapshot.val();
+    var outputstring = "";
+    for (var key in ad_list) {
+      var ad = ad_list[key];
+      console.log(ad["body"]);
+      var outputstring = outputstring + "<div class = 'postedAd'>";
+      var headline = ad["title"];
+      var content = ad["body"];
+      var outputstring = outputstring + "<h1>" + headline + "</h1>";
+      var outputstring = outputstring + "<p>" + content + "</p>" + "</div>";
+    }
+    var box = document.getElementById("adbox");
+    box.innerHTML = outputstring;
+  })
+
   $("#post").click(function(){
     var adTitle = document.getElementById("title").value;
     var adbody = document.getElementById("body").value;
