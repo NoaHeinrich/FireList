6d05b77181d6293d06661ced2fd8709ad10c9205
Noah Heinrich <noahheinrich@gmail.com>
2017-04-11 10:42:25 -0500

    Add method to format outputstring

--- public/script.js
+++ public/script.js
@@ -4,18 +4,23 @@ $(document).ready(function(){
     var outputstring = "";
     for (var key in ad_list) {
       var ad = ad_list[key];
-      console.log(ad["body"]);
-      var outputstring = outputstring + "<div class = 'postedAd'>";
+      // var outputstring = outputstring + "<div class = 'postedAd'>";
       var headline = ad["title"];
       var content = ad["body"];
       var posted = ad["dayPosted"];
-      var outputstring = outputstring + "<h1>" + headline + "</h1>";
-      var outputstring = outputstring + "<p>" + posted + "</p>";
-      var outputstring = outputstring + "<p>" + content + "</p>" + "</div><hr>" ;
+      outputstring = makeDiv(outputstring, headline, content, posted);
+      // var outputstring = outputstring + "<h1>" + headline + "</h1>";
+      // var outputstring = outputstring + "<p>" + posted + "</p>";
+      // var outputstring = outputstring + "<p>" + content + "</p>" + "</div><hr>" ;
     }
     var box = document.getElementById("adbox");
     box.innerHTML = outputstring;
   })
+
+  function makeDiv (string, h, c, p) {
+    var string = string + "<div class='postedAd'><h1>" +h + "</h1><p>" + p + "</p><p>" + c + "</p></div><hr>";
+    return string;
+  }
 
   $("#post").click(function(){
     var adTitle = document.getElementById("title").value;
